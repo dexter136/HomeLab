9763fda1d276abf2eadb3fa4eebe00f3e37fbad0
Sean <aisling@dex136.xyz>
2024-04-11 23:06:49 -0400

    (app) add poddownloader

--- 310-external-cluster-dependencies/secrets.tf
+++ 310-external-cluster-dependencies/secrets.tf
@@ -8,3 +8,23 @@ resource "kubernetes_secret" "discord-secret" {
     "DISCORD_WEBHOOK" = var.DISCORD_WEBHOOK
   }
 }
+
+resource "kubernetes_secret" "podcastdownload" {
+  metadata {
+    name      = "podcastdownload-secret"
+    namespace = "media"
+  }
+
+  data = {
+    "PODCAST_0"  = var.PODCAST_0
+    "PODCAST_1"  = var.PODCAST_1
+    "PODCAST_2"  = var.PODCAST_2
+    "PODCAST_3"  = var.PODCAST_3
+    "PODCAST_4"  = var.PODCAST_4
+    "FILEPATH_0" = var.FILEPATH_0
+    "FILEPATH_1" = var.FILEPATH_1
+    "FILEPATH_2" = var.FILEPATH_2
+    "FILEPATH_3" = var.FILEPATH_3
+    "FILEPATH_4" = var.FILEPATH_4
+  }
+}
--- 310-external-cluster-dependencies/variables.tf
+++ 310-external-cluster-dependencies/variables.tf
@@ -20,3 +20,53 @@ variable "pihole_password" {
   type      = string
   sensitive = true
 }
+
+variable "PODCAST_0" {
+  type      = string
+  sensitive = true
+}
+
+variable "PODCAST_1" {
+  type      = string
+  sensitive = true
+}
+
+variable "PODCAST_2" {
+  type      = string
+  sensitive = true
+}
+
+variable "PODCAST_3" {
+  type      = string
+  sensitive = true
+}
+
+variable "PODCAST_4" {
+  type      = string
+  sensitive = true
+}
+
+variable "FILEPATH_0" {
+  type      = string
+  sensitive = true
+}
+
+variable "FILEPATH_1" {
+  type      = string
+  sensitive = true
+}
+
+variable "FILEPATH_2" {
+  type      = string
+  sensitive = true
+}
+
+variable "FILEPATH_3" {
+  type      = string
+  sensitive = true
+}
+
+variable "FILEPATH_4" {
+  type      = string
+  sensitive = true
+}
