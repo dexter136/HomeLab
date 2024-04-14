02c7d9f467a98065a86dc9aa5c310a455e7d947c
Sean <aisling@dex136.xyz>
2024-04-14 15:29:39 -0400

    (fix) tf plan output filename

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -25,7 +25,7 @@ if $external; then
         terraform -chdir="./terraform/cloudflare" apply -var-file=../../configs/generated_configs/external.tfvars -auto-approve
     else
         terraform -chdir="./terraform/cloudflare" plan -var-file=../../configs/generated_configs/external.tfvars -no-color > configs/generated_configs/cloudflare.tfplan
-        cat configs/generated_configs/310.tfplan
+        cat configs/generated_configs/cloudflare.tfplan
     fi
 fi
 if $cluster; then
@@ -38,6 +38,6 @@ if $cluster; then
         terraform -chdir="./terraform/talos" apply -var-file=../../configs/generated_configs/cluster-build.tfvars -auto-approve
     else
         terraform -chdir="./terraform/talos" plan -var-file=../../configs/generated_configs/cluster-build.tfvars -no-color > configs/generated_configs/talos.tfplan
-        cat configs/generated_configs/100.tfplan
+        cat configs/generated_configs/talos.tfplan
     fi
 fi
