178a6a6c048534baf1ac3176caa4138ec1c70a53
Sean <aisling@dex136.xyz>
2024-04-14 11:04:04 -0400

    (feat) Move to new cloudflare_ruleset terraform
    https://developers.cloudflare.com/waf/reference/migration-guides/firewall-rules-to-custom-rules/#relevant-changes-for-terraform-users

--- terraform/external-cluster-dependencies/cloudflare.tf
+++ terraform/external-cluster-dependencies/cloudflare.tf
@@ -14,43 +14,29 @@ locals {
   ]
 }
 
-resource "cloudflare_filter" "countries" {
-  zone_id     = data.cloudflare_zone.dex136.id
-  description = "Expression to block all countries except US and CA"
-  expression  = "(ip.geoip.country ne \"US\" and ip.geoip.country ne \"CA\")"
-}
-
-resource "cloudflare_firewall_rule" "countries" {
-  zone_id     = data.cloudflare_zone.dex136.id
-  description = "Firewall rule to block all countries except US and CA"
-  filter_id   = cloudflare_filter.countries.id
-  action      = "block"
-}
-
-resource "cloudflare_filter" "bots" {
-  zone_id     = data.cloudflare_zone.dex136.id
-  description = "Expression to block bots determined by CF"
-  expression  = "(cf.client.bot)"
-}
-
-resource "cloudflare_firewall_rule" "bots" {
-  zone_id     = data.cloudflare_zone.dex136.id
-  description = "Firewall rule to block bots determined by CF"
-  filter_id   = cloudflare_filter.bots.id
-  action      = "block"
-}
-
-resource "cloudflare_filter" "threats" {
-  zone_id     = data.cloudflare_zone.dex136.id
-  description = "Expression to block medium threats"
-  expression  = "(cf.threat_score gt 14)"
-}
-
-resource "cloudflare_firewall_rule" "threats" {
-  zone_id     = data.cloudflare_zone.dex136.id
-  description = "Firewall rule to block medium threats"
-  filter_id   = cloudflare_filter.threats.id
-  action      = "block"
+resource "cloudflare_ruleset" "firewall" {
+  kind    = "zone"
+  name    = "default"
+  phase   = "http_request_firewall_custom"
+  zone_id = data.cloudflare_zone.dex136.id
+  rules {
+    action      = "block"
+    description = "Firewall rule to block bots determined by CF"
+    enabled     = true
+    expression  = "(cf.client.bot)"
+  }
+  rules {
+    action      = "block"
+    description = "Firewall rule to block medium threats"
+    enabled     = true
+    expression  = "(cf.threat_score gt 14)"
+  }
+  rules {
+    action      = "block"
+    description = "Firewall rule to block all countries except US and CA"
+    enabled     = true
+    expression  = "(ip.geoip.country ne \"US\" and ip.geoip.country ne \"CA\")"
+  }
 }
 
 resource "cloudflare_api_token" "system_api_token" {
