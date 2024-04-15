5bef56e399970122665fe9f9de0862e4da463a8e
Sean <aisling@dex136.xyz>
2024-04-14 20:45:34 -0400

    (feat) Encrypt terraform vars with sops

--- scripts/generate_configs.sh
+++ scripts/generate_configs.sh
@@ -2,3 +2,5 @@
 
 gomplate -f configs/templates/machineconfig.yaml.tmpl
 gomplate -f configs/templates/cluster-build.tfvars.tmpl
+
+sops --decrypt configs/external.tfvars.json > configs/generated_configs/external.tfvars.json
