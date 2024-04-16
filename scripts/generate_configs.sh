b058b02a51963801c0a9c4493d6bebfe0d14b9b6
Aisling <aisling@dex136.xyz>
2024-04-16 13:43:40 -0400

    (feat) Update generate config script to get talos schematic

--- scripts/generate_configs.sh
+++ scripts/generate_configs.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+#Get talos schematic ID and update inventory.yaml
+resp=$(curl -X POST --data-binary @configs/schematic.yaml https://factory.talos.dev/schematics)
+schematic=$(echo $resp | jq -r '.id')
+yq -i ".talos_factory_key = \"$schematic\"" configs/inventory.yaml
+
+#Build gomplate templates
 gomplate -f configs/templates/machineconfig.yaml.tmpl
 gomplate -f configs/templates/cluster-build.tfvars.tmpl
 
+#Decrypt tfvars
 sops --decrypt configs/external.tfvars.json > configs/generated_configs/external.tfvars.json
