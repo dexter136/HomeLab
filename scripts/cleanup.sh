51ec9ff70231c24a0e8d001f6eaa9f1c3199db60
Aisling <aisling@dex136.xyz>
2024-04-17 01:11:41 -0400

    (feat) Update cleanup script to remove files created by kustomize builds

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-find . -name '*generated_configs/*'
-find . -name '*generated_configs/*' | xargs rm -rf
-
-find . -name '*.terraform*'
+find -type d -name generated_configs | xargs rm -rf
+find -type d -name charts | xargs rm -rf
 find . -name '*.terraform*' | xargs rm -rf
