6b70ea1e2d3c6f55396bc1eb31b2a4b20f3e9707
Aisling McGinn <aisling@dex136.xyz>
2025-02-02 13:13:53 -0500

    feat(app): use per app postgres db

--- scripts/updatenodecount.sh
+++ scripts/updatenodecount.sh
@@ -6,7 +6,4 @@ nodecount=$(yq '.nodes | length' configs/inventory.yaml)
 echo "Found $nodecount nodes in inventory."
 
 echo "Updating fstrim parellism."
-yq -i ".controllers.fstrim.cronjob.parallelism = $nodecount" cluster/kube-system/fstrim/helmvalues.yaml
-
-echo "Updating postgres cluster replicas."
-yq -i "select(.kind == \"Cluster\").spec.instances = $nodecount" cluster/cluster-software/database/cluster.yaml
+yq -i ".app-template.controllers.fstrim.cronjob.parallelism = $nodecount" cluster/kube-system/fstrim/values.yaml
